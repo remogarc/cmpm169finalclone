5fa32b544c84b08dad0ae045ee5f18e7e7f1e71c
prjktalix <briancamilo0720@gmail.com>
2024-03-03 18:07:46 -0800

    added particle

--- FinalProject/js/sketch.js
+++ FinalProject/js/sketch.js
@@ -11,6 +11,16 @@ let cat;
 let rat;
 let ratDead = false;
 let ratsCaught = [];
+let particles = [];
+
+let colorScheme = ['#FF0000',
+	'#FF7F00',
+	'#FFFF00',
+	'#00FF00',
+	'#0000FF',
+	'#4B0082',
+  '#9400D3'
+];
 
 function preload() {
   let regCatImg = loadImage("../img/cat.png");
@@ -106,6 +116,8 @@ function draw() {
     ratDead = true;
     setTimeout(delayRespawn, 3000);
   }
+
+  updateAndDisplayTrail();
 }
 
 function delayRespawn() {
@@ -118,6 +130,8 @@ class Animal {
     this.y = y + canvasH / 2;
     this.img = img;
     this.id = id;
+    this.prevX = this.x;
+    this.prevY = this.y;
     this.radius = animalWidth / 4;
     this.speedX = random(possibleSpeeds);
     this.speedY = random(possibleSpeeds);
@@ -127,6 +141,9 @@ class Animal {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
+
+    this.prevX = this.x;
+    this.prevY = this.y;
 
     // Bounce off the edges
     if (this.x + animalWidth / 4 >= canvasW || this.x - animalWidth / 4 <= 0) {
@@ -189,5 +206,50 @@ class Animal {
     let newMusic = catInfo[newCatType]["music"];
     newMusic.play();
     newMusic.setLoop(true);
+  }
+
+}
+
+
+function Particle(x, y, vx, vy) {
+  this.pos = createVector(x, y);
+  this.vel = createVector(vx, vy);
+  this.acc = createVector(0, 0);
+  this.lifespan = 255;
+  this.color = random(colorScheme);
+
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.lifespan -= 4;
+  };
+
+  this.display = function() {
+    noStroke();
+    fill(this.color + hex(this.lifespan, 2));
+    ellipse(this.pos.x, this.pos.y, 12); // Adjust size as needed
+  };
+
+  this.isDead = function() {
+    return this.lifespan < 0;
+  };
+}
+
+function updateAndDisplayTrail() {
+  let direction = createVector(cat.x - cat.prevX, cat.y - cat.prevY).mult(-1);
+  direction.normalize();
+
+  for (let i = 0; i < 5; i++) {
+    let angle = direction.heading() + random(-PI / 6, PI / 6);
+    let speed = random(3, 8);
+    particles.push(new Particle(cat.x - canvasW / 2, cat.y - canvasH / 2, cos(angle) * speed, sin(angle) * speed));
+  }
+
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].display();
+    if (particles[i].isDead()) {
+      particles.splice(i, 1);
+    }
   }
 }
